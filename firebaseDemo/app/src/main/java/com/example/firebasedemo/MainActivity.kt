cfcce39126c4615b2944db8b53b8f200b13a6e2f
Joey yang <yty0606@gmail.com>
2022-05-17 15:27:39 +0800

    Feat: Join anonymous login code
    Solution:
    1. As title. (But it still needs some modification to become MVVM style.)
    
    Author: Joey

--- firebaseDemo/app/src/main/java/com/example/firebasedemo/MainActivity.kt
+++ firebaseDemo/app/src/main/java/com/example/firebasedemo/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.firebasedemo
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
@@ -15,20 +17,28 @@ import androidx.navigation.compose.rememberNavController
 import com.example.firebasedemo.ui.theme.FirebaseDemoTheme
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 /*
     Author: Joey yang
     Note:
     1. About join firebase into project, you can refer to udemy class.
+       Tools > Firebase. (在 Firebase 的 "Dashboard" 可以看到本專案)
+    2. Anonymous login official document.
+       https://firebase.google.com/docs/auth/android/anonymous-auth#kotlin+ktx
  */
 class MainActivity : ComponentActivity() {
     private lateinit var analytics: FirebaseAnalytics
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             analytics = Firebase.analytics
+            auth = Firebase.auth
+
             FirebaseDemoTheme {
                 val navController = rememberNavController() //Navigation Step2
 
@@ -38,7 +48,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     composable("login_screen") {
                         //first screen
-                        LoginScreen01(navController = navController)
+                        LoginScreen01(navController = navController, auth, this@MainActivity)
                     }
                     composable("shop_screen") {
                         //second screen
@@ -51,12 +61,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun LoginScreen01(navController: NavController) {
+fun LoginScreen01(navController: NavController, auth: FirebaseAuth, activity: MainActivity) {
     Text(text = "First screen",
         modifier = Modifier.clickable(onClick = {
             navController.navigate("shop_screen")
         })
     )
+    auth.signInAnonymously()
+        .addOnCompleteListener(activity) { task ->
+            if (task.isSuccessful) {
+                // Sign in success, update UI with the signed-in user's information
+                Log.d("TAG", "signInAnonymously:success")
+                val user = auth.currentUser
+                Toast.makeText(activity.baseContext, "signInAnonymously:success, ${user.toString()}",
+                    Toast.LENGTH_SHORT).show()
+                //updateUI(user)
+            } else {
+                // If sign in fails, display a message to the user.
+                Log.w("TAG", "signInAnonymously:failure", task.exception)
+                Toast.makeText(activity.baseContext, "Authentication failed.",
+                    Toast.LENGTH_SHORT).show()
+                //updateUI(null)
+            }
+        }
+
+    //TODO: Need to join viewModel and remember State to fit MVVM style
 }
 
 @Composable
@@ -73,6 +102,6 @@ fun ShopScreen02(navController: NavController) {
 @Composable
 fun DefaultPreview() {
     FirebaseDemoTheme {
-        LoginScreen01(rememberNavController())
+        //LoginScreen01(rememberNavController())
     }
 }
