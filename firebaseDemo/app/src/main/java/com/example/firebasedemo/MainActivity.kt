8dc9f9ae14cacc1e3cfd366e8559f647da8a6452
Joey yang <yty0606@gmail.com>
2022-05-17 10:56:11 +0800

    Feat: Join Navigation function
    Solution:
    1. As title.
    
    Author: Joey

--- firebaseDemo/app/src/main/java/com/example/firebasedemo/MainActivity.kt
+++ firebaseDemo/app/src/main/java/com/example/firebasedemo/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.firebasedemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.clickable
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.firebasedemo.ui.theme.FirebaseDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +19,20 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FirebaseDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController() //Navigation Step2
+
+                NavHost(
+                    navController = navController,
+                    startDestination = "login_screen"
                 ) {
-                    Greeting("Android")
+                    composable("login_screen") {
+                        //first screen
+                        LoginScreen01(navController = navController)
+                    }
+                    composable("shop_screen") {
+                        //second screen
+                        ShopScreen02(navController = navController)
+                    }
                 }
             }
         }
@@ -30,14 +40,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LoginScreen01(navController: NavController) {
+    Text(text = "First screen",
+        modifier = Modifier.clickable(onClick = {
+            navController.navigate("shop_screen")
+        })
+    )
 }
+
+@Composable
+fun ShopScreen02(navController: NavController) {
+    Text(text = "Second screen",
+        modifier = Modifier.clickable(onClick = {
+            navController.navigate("login_screen")
+        })
+    )
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     FirebaseDemoTheme {
-        Greeting("Android")
+        LoginScreen01(rememberNavController())
     }
 }
